62d5e87186542abbcc5c45e8bd160311d08e04c0
Prince K <princephpdev@gmail.com>
2023-02-20 14:49:41 +0530

    Fix Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
-import { Container, Heading, IconButton, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Center,
+  Container,
+  Flex,
+  Heading,
+  Highlight,
+  IconButton,
+  SimpleGrid,
+  Spacer,
+  Text,
   useToast,
 } from "@chakra-ui/react";
 import {
